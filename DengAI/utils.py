8df08c9bdf4ef991f6e6cb8aa91495f781a754d5
yistLin <yishen992@gmail.com>
2017-05-17 03:34:03 +0800

    the very basic functions and classes

---
+++ DengAI/utils.py
@@ -0,0 +1,17 @@
+from pandas import read_csv
+
+class DataReader:
+    feature_headers = ['city', 'year', 'weekofyear', 'week_start_date', 'ndvi_ne', 'ndvi_nw', 'ndvi_se', 'ndvi_sw', 'precipitation_amt_mm', 'reanalysis_air_temp_k', 'reanalysis_avg_temp_k', 'reanalysis_dew_point_temp_k', 'reanalysis_max_air_temp_k', 'reanalysis_min_air_temp_k', 'reanalysis_precip_amt_kg_per_m2', 'reanalysis_relative_humidity_percent', 'reanalysis_sat_precip_amt_mm', 'reanalysis_specific_humidity_g_per_kg', 'reanalysis_tdtr_k', 'station_avg_temp_c', 'station_diur_temp_rng_c', 'station_max_temp_c', 'station_min_temp_c', 'station_precip_mm']
+    label_headers = ['city', 'year', 'weekofyear', 'total_cases']
+
+    @classmethod
+    def read_features(cls, file_path):
+        data_frame = read_csv(file_path)
+        # handle NaN
+        data_frame = data_frame.fillna(0.0)
+        return data_frame.values
+
+    @classmethod
+    def read_labels(cls, file_path):
+        data_frame = read_csv(file_path)
+        return data_frame['total_cases'].values
